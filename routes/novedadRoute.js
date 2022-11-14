e4199b2ed6ccaf09c26c81c0c5ff2a2e8c4f0947
mv <matias.vilche1901@alumnos.ubiobio.cl>
2022-11-14 00:35:59 -0300

    Agrega request de novedad especifica

--- routes/novedadRoute.js
+++ routes/novedadRoute.js
@@ -6,5 +6,6 @@ const novedadController = require('../controllers/novedadController');
 api.post('/', novedadController.createNovedad);
 api.get('/', novedadController.getNovedades);
 api.put('/:id', novedadController.updateNovedad);
+api.get('/:id', novedadController.getNovedad);
 
 module.exports = api;
