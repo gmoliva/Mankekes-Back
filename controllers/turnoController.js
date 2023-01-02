aaae89dc895f230babb1baff5756cca09eee1620
gmoliva <gmo.olivap@gmail.com>
2023-01-02 03:07:03 -0300

    validacion de turnos por semana

--- controllers/turnoController.js
+++ controllers/turnoController.js
@@ -16,7 +16,7 @@ const validateTurno = (req, res) => {
 		//console.log(turno)
 
 		if(!turno)
-		return sendTurno(req, res);
+		return validateWeek(req, res);
 		else
 		return res.status(400).send({msg: "existe turno en la misma fecha"})
 
@@ -140,6 +140,67 @@ const setEntradaSalida = async (req, res) => {
 	})
 }
 
+const validateWeek = async (req, res) => {
+
+	const {
+		tipo,
+		idUsuario,
+		horaEntrada,
+		horaSalida,
+		fecha
+	} = req.body;
+
+	// console.log(fecha)
+	// let lastWeek = new Date(Date.parse(fecha) - 7 * 60 * 60 * 24 * 1000)
+	// console.log("last week day (-7) is " + lastWeek)
+
+	let turnoDate = new Date(Date.parse(fecha))
+
+	let last3days = new Date(turnoDate - 3 * 60 * 60 * 24 * 1000)
+	// console.log("last 3 days limit is " + last3days)
+
+	let next3days = new Date(Date.parse(turnoDate) + 3 * 60 * 60 * 24 * 1000)
+	// console.log("next 3 days limit is " + next3days)
+
+	let startOfWeek = new Date(turnoDate - turnoDate.getDay() * 60 * 60 * 24 * 1000)
+	// console.log("start of week (sunday) is " + startOfWeek)
+	
+	let endOfWeek = new Date(Date.parse(startOfWeek) + 7 * 60 * 60 * 24 * 1000)
+	// console.log("end of week (saturday) is " + endOfWeek)
+
+	let weekCount = 0;
+	let daysCount = 0;
+
+	Turno.find()
+	.where('idUsuario').equals(idUsuario)
+	.exec((err, result) => 
+	{
+		if (err) console.log("err: "+err)
+
+		result.forEach(turno => {
+			if(startOfWeek <= Date.parse(turno.fecha) && endOfWeek >=  Date.parse(turno.fecha))
+			weekCount++;
+
+			if(last3days <= Date.parse(turno.fecha) && next3days >=  Date.parse(turno.fecha))
+			daysCount++;
+		});
+
+		// console.log(result)
+		// console.log("turnos this week: " + weekCount)
+		// console.log("turnos last 3 days: " + daysCount)
+
+		if(weekCount<3){
+			if(daysCount<3)
+			return sendTurno(req, res)
+			else
+			return res.status(202).send({msg: "Conserje no puede tener turnos asignados más de 3 días seguidos."})
+		}
+		else
+		return res.status(202).send({msg: "Conserje ya tiene 3 turnos asignados en la semana seleccionada."})
+
+	})
+
+}
 
 
 
